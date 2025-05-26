547730334f827c352aab6e87c945d8f1d8ccc630
mkhong210 <mkhong210@gmail.com>
2025-05-26 17:37:26 +0900

    chore: Vite + React + Tailwind 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +22,13 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+      <div className="text-2xl text-blue-500 font-bold p-4">
+        Tailwind 적용 완료 🎉
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
