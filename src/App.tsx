9250b8c0525d7d550feed98be1d7b0e05a15ac03
mkhong210 <mkhong210@gmail.com>
2025-05-26 19:49:36 +0900

    chore: 라우터 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
 
 function App() {
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+      </Routes>
 
       <div className="text-2xl text-blue-500 font-bold p-4">
         Tailwind 적용 완료 🎉
