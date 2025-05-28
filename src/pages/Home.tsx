0bc478f0fd24597d4c5a2a399646ec09f2826ae8
mkhong210 <mkhong210@gmail.com>
2025-05-28 15:22:32 +0900

    feat: Home 페이지 생성 및 메인 레이아웃 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,80 @@
+import { useNavigate } from "react-router-dom";
 import AccountSelect from "../components/AccountSelect";
 import CalendarView from "../components/CalendarView";
-import Header from "../components/Header";
 import ListView from "../components/ListView";
 import Monthly from "../components/Monthly";
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useState } from "react";
+import { getAccounts } from "../utils/accounts";
+import { MergedTransactions } from "../hooks/MergedTransactions";
+import AddButton from "../components/AddButton";
 
 const Home: React.FC = () => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  const [accounts, setAccounts] = useState<{ id: string; name: string }[]>([]);
+  const [selectedAccountId, setSelectedAccountId] = useState<string>("");
+
+  // 월 단위
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+
+  // 로그인 유무 확인
+  useEffect(() => {
+    if (!loading && !user) {
+      navigate("/login");
+    }
+  }, [loading, user, navigate]);
+
+  // 로그인 후 계좌 불러오기
+  useEffect(() => {
+    if (!user) return;
+
+    const fetchAccounts = async () => {
+      const data = await getAccounts(user.id);
+      setAccounts(data);
+      if (data.length > 0) {
+        setSelectedAccountId(data[0].id);
+      }
+    };
+
+    fetchAccounts();
+  }, [user]);
+
+  const merged = MergedTransactions(selectedAccountId, selectedMonth);
+
+  if (loading) return <p>로딩 중...</p>;
+
   return (
     <>
-      <Header></Header>
       <section className="container_wrap">
+        {/* {user ? <p>환영합니다, {user.email}!</p> : <p>로그인이 필요합니다.</p>} */}
+
         <AccountSelect
-        // accounts={accounts}
-        // selectedAccountId={selectedAccountId}
-        // setSelectedAccountId={setSelectedAccountId}
+          accounts={accounts}
+          selectedAccountId={selectedAccountId}
+          setSelectedAccountId={setSelectedAccountId}
         />
-        <Monthly
-        // selectedAccountId={selectedAccountId}
-        // selectedDate={selectedDate}
-        />
+
+        {/* <Monthly
+          selectedAccountId={selectedAccountId}
+          selectedMonth={selectedMonth}
+          setSelectedMonth={setSelectedMonth}
+        /> */}
+
         <div className="flex flex-col md:flex-row gap-6">
-          {/* <div className="md:w-1/2"> */}
           <div className="grow-6">
             <CalendarView
-            // accounts={accounts}
-            // selectedAccountId={selectedAccountId}
-            // selectedDate={selectedDate}
-            // setSelectedDate={setSelectedDate}
+              mergedRecords={merged}
+              selectedMonth={selectedMonth}
+              setSelectedMonth={setSelectedMonth}
             />
           </div>
           <div className="md:w-1/2">
-            <ListView
-            // accounts={accounts}
-            // selectedAccountId={selectedAccountId}
-            />
+            <ListView mergedRecords={merged} />
           </div>
         </div>
+        <AddButton accountId={selectedAccountId}></AddButton>
       </section>
     </>
   );
