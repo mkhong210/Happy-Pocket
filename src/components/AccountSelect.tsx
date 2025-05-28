6355126eb4cfd12d2d36372d1f66165eabd31406
mkhong210 <mkhong210@gmail.com>
2025-05-28 15:19:23 +0900

    feat: AccountSelect 컴포넌트 생성 및 계좌 선택 UI 구현

--- src/components/AccountSelect.tsx
+++ src/components/AccountSelect.tsx
@@ -1,13 +1,30 @@
 import type React from "react";
 
-const AccountSelect: React.FC = () => {
+interface Props {
+  accounts: { id: string; name: string }[];
+  selectedAccountId: string;
+  setSelectedAccountId: (id: string) => void;
+}
+
+const AccountSelect: React.FC<Props> = ({
+  accounts,
+  selectedAccountId,
+  setSelectedAccountId,
+}) => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 bg-white">
       <select
         aria-label="계좌 선택"
-        value={"아이디"}
-        className="w-full p-2 border rounded focus:outline-none focus:ring-2 focu	:ring-yellow-500"
-      ></select>
+        value={selectedAccountId}
+        onChange={(e) => setSelectedAccountId(e.target.value)}
+        className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-yellow-500"
+      >
+        {accounts.map((acc) => (
+          <option key={acc.id} value={acc.id}>
+            {acc.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
