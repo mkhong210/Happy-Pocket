6db8cf33c53a61727b0aa36d06e338108f91bad7
mkhong210 <mkhong210@gmail.com>
2025-05-28 15:12:05 +0900

    feat: 계좌(account) 관련 유틸 함수 추가

--- src/utils/accounts.ts
+++ src/utils/accounts.ts
@@ -15,25 +15,3 @@ export async function getAccounts(userId: string) {
 
   return data;
 }
-
-// // 계좌 데이터 가져오기
-// export const getAccounts = (): Account[] => {
-//   const data = loadFromLocalStorage();
-//   return data.accounts;
-// };
-
-// // 특정 계좌의 거래 내역 가져오기
-// export const getRecords = (accountId: string): RecordItem[] => {
-//   const data = loadFromLocalStorage();
-//   const account = data.accounts.find((acc) => acc.id === accountId);
-//   return account ? account.records : [];
-// };
-
-// // 특정 계좌의 반복 거래 가져오기
-// export const getRegularTransactions = (
-//   accountId: string
-// ): RegularTransaction[] => {
-//   const data = loadFromLocalStorage();
-//   const account = data.accounts.find((acc) => acc.id === accountId);
-//   return account ? account.regular : [];
-// };
