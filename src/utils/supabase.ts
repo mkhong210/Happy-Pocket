fcdce71d9e5f37efa7c794099b6fd08d72b34221
mkhong210 <mkhong210@gmail.com>
2025-05-28 14:53:05 +0900

    chore: Supabase client 설정

---
+++ src/utils/supabase.ts
@@ -0,0 +1,8 @@
+// supabase.ts
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../../types_db";
+
+export const supabase = createClient<Database>(
+  import.meta.env.VITE_SUPABASE_URL!,
+  import.meta.env.VITE_SUPABASE_ANON_KEY!
+);
